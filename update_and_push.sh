8659337f77e3863c9c2118d28b38faeaba534d63
lynn pepin <lynnpepin@tutanota.com>
2023-11-15 18:45:34 -0500

    feat: Add a convenience tool to build, push, and update. Let's see if this works

---
+++ update_and_push.sh
@@ -0,0 +1,17 @@
+# Update posts
+zola build
+
+# Get commit message and push
+read -p "Enter commit message: " commit_message
+if [ -z "$commit_message" ]; then
+    echo "Commit message is empty; exiting."
+    exit 1
+fi
+
+git add .
+git commit -m "$commit_message"
+git push gh
+git push gl
+
+# Update website
+ssh face 'bash ~/updatesite.sh'
